48f6582e30ceba14fb9aece0f885cc997a683e13
madxsuz <agmanzanares.chmsu@gmail.com>
2025-02-13 01:03:31 +0800

    fix error configs

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/my-riri/' : '/', // or '/' for user page
+export default defineConfig({
   plugins: [react()],
-}));
+})
